59e0aceba1c2d612fcfaba5b668fa6abfd6ac759
Vimal <vm.21cs48@thdcihet.ac.in>
2024-08-15 12:06:02 +0530

    basic model setup

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose,{Schema} from 'mongoose'
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+const videoSchema=new Schema({
+ videoFile:{
+    type:String,
+    required:true
+ },
+ thumbnail:{
+    type:String,
+    required:true
+ },
+ title:{
+    type:String,
+    required:true
+ },
+ decription:{
+    type:String,
+    required:true
+ },
+ duration:{//this will come from cloudinary
+    type:Number,
+    required :true,
+ },
+ views:{
+    type:Number,
+    default:0
+ },
+ isPublished:{
+    type:Boolean,
+    default:true
+ },
+ owner:{
+    type:Schema.Types.ObjectId,
+    ref:"User"
+ }
+
+
+})
+videoSchema.plugin(mongooseAggregatePaginate)
+const Video=mongoose.model("Video",videoSchema)
+export default Video
