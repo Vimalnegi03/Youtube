6084d24471de2c6e65595b53f0175d79ecd4b139
Vimal <vm.21cs48@thdcihet.ac.in>
2024-08-18 12:33:28 +0530

    sub pipeline added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import ApiError from "../utils/ApiError.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import jwt from 'jsonwebtoken'
+import mongoose from "mongoose";
 
 const generateAcess_and_refreshToken=async(userId)=>{
     try {
@@ -316,4 +317,48 @@ export const getUserChannelProfile=asyncHandler(async(req,res)=>{
     throw new ApiError(404,"channel does not exist")
     }
     return res.status(200).json(new ApiResponse(200,channel[0],"user channel fetched successfully"))
+})
+
+
+export const getWatchHistory=asyncHandler(async(req,res)=>{
+    const user=await User.aggregate([{
+        $match:{
+            _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+    },{
+        $lookup:{
+            from:"videos",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+                {
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[
+                            {
+                                $project:{
+                                      fullName:1,
+                                      username:1,
+                                      avatar:1
+                                }
+                            }
+                        ]
+                    }
+                },{
+                    $addFields:{
+                        owner:{
+                            $first:"owner",
+
+                        }
+                    }
+                }
+            ]
+        }
+    },])
+
+    return res.status(200).json(new ApiResponse(200,user[0].watchHistory,"watch history fetched successfully"))
 })
