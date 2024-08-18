380f933b5a1212241d44c5db0eb96032b3b9e198
Vimal <vm.21cs48@thdcihet.ac.in>
2024-08-18 12:52:10 +0530

    routes updated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 //for file handling
 import {upload} from "../middlewares/multer.middleware.js";
 
@@ -23,4 +23,11 @@ userRouter.route("/login").post(loginUser)
 //secured methods
 userRouter.route("/logout").post(verifyJWT,logoutUser)
 userRouter.route("refresh-token").post(refreshAccessToken)
+userRouter.route("/changePassword").post(verifyJWT,changeCurrentPassword)
+userRouter.route("/current-user").get(verifyJWT,getCurrentUser)
+userRouter.route("/update-account").patch(verifyJWT,updateAccountDetails)
+userRouter.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+userRouter.route("/coverImageupdate").patch(verifyJWT.upload.single("coverImage"),updateUserCoverImage)
+userRouter.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+userRouter.route("/history").get(verifyJWT,getWatchHistory)
 export default userRouter
